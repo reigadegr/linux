751443bef06c0d34387515bc1cf1289abab0ba82
Peter Jung <admin@ptr1337.dev>
2025-11-24 14:41:11 +0100

    t2
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- drivers/gpu/drm/i915/display/intel_quirks.h
+++ drivers/gpu/drm/i915/display/intel_quirks.h
@@ -21,6 +21,7 @@ enum intel_quirk_id {
 	QUIRK_NO_PPS_BACKLIGHT_POWER_HOOK,
 	QUIRK_FW_SYNC_LEN,
 	QUIRK_EDP_MAX_240HZ_HOOK,
+	QUIRK_DDI_A_FORCE_4_LANES,
 };
 
 void intel_init_quirks(struct intel_display *display);
