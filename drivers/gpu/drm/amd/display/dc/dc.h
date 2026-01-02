cab97d2e114bcd64df44eb372a72a9696b644f79
Peter Jung <admin@ptr1337.dev>
2026-01-02 16:53:15 +0100

    handheld
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- drivers/gpu/drm/amd/display/dc/dc.h
+++ drivers/gpu/drm/amd/display/dc/dc.h
@@ -60,7 +60,7 @@ struct dmub_notification;
 /**
  * MAX_SURFACES - representative of the upper bound of surfaces that can be piped to a single CRTC
  */
-#define MAX_SURFACES 4
+#define MAX_SURFACES 6
 /**
  * MAX_PLANES - representative of the upper bound of planes that are supported by the HW
  */
